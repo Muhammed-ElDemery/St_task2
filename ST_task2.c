a7405c0d840bd3a60d207899b713667e33ea448b
Mohamed El-Demairy <113904197+Muhammed-ElDemery@users.noreply.github.com>
2024-01-31 16:00:10 +0200

    ST_task2.c

--- ST_task2.c
+++ ST_task2.c
@@ -23,16 +23,3 @@ int main () {
     }
     return 0;
 }
-/*
-    test@linux-test:~/task2$ ./a.out
-    Al-salam alaykom ya habeeb > ST microelectronics training
-    you said: ST microelectronics training
-    Al-salam alaykom ya habeeb > linux programming training
-    you said: linux programming training
-    Al-salam alaykom ya habeeb > now we will try to exit
-    you said: now we will try to exit
-    Al-salam alaykom ya habeeb > Exit
-    Good Bye:)
-    test@linux-test:~/task2$
-
-*/
